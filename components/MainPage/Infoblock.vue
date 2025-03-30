30d1e0d29d9e0a14834452d7f49fcf38294a884c
hunamuna123 <95960372+hunamuna123@users.noreply.github.com>
2025-03-30 11:53:23 +0300

    ythjg

--- components/MainPage/Infoblock.vue
+++ components/MainPage/Infoblock.vue
@@ -1,34 +1,30 @@
 <template>
-  <div class="max-w-[85rem] w-full mx-auto py-4 min-h-screen ">
-    <div class="flex flex-col md:flex-row items-start justify-between rounded-xl bg-gradient-to-br from-teal-600 to-blue-600 p-6 mt-16">
-      <div class="px-4 sm:px-6 lg:px-8">
-  <div class="grid lg:grid-cols-7 lg:gap-x-8 xl:gap-x-12 lg:items-start">
-    <div class="lg:col-span-3 flex flex-col min-h-[600px]">
+  <div class="max-w-[85rem] w-full mx-auto py-4 min-h-screen flex flex-col justify-center">
+    <!-- Блок с фоном, фиксированная высота 600px -->
+    <div class="flex flex-col items-center justify-center h-[600px] text-center rounded-xl bg-gradient-to-br from-teal-600 to-blue-600 p-6 bg-[url(/infoblock/bg.png)] bg-cover bg-center">
+      <div class="px-4 sm:px-6 lg:px-8 w-full">
+        <div class="flex flex-col items-center text-center">
+          <h1 class="text-[clamp(50px,15vw,250px)] text-white font-extrabold leading-none" style="font-family: fat;">Flowly</h1>
+          <h2 class="text-[clamp(30px,5vw,100px)] text-white font-bold mt-4" style="font-family: fat;">АНАЛИЗ КОНКУРЕНТОВ</h2>
+        </div>
+      </div>
     </div>
 
-
-
-    
-  </div>
-</div>
-    </div>
+    <!-- Блок с преимуществами -->
     <div class="flex flex-wrap gap-4 justify-center mt-6">
-      <div class=" flex items-center  bg-flowlybg2 p-4 rounded-xl flex-[1_1_calc(25%-1rem)] min-w-[200px]">
-        <img src="/infoblock/4.png" class="max-w-[64px] max-h-[64px]" alt="">
-        <p class="text-lg text-white ml-4">Распознает и генерирует голос</p>
-      </div>
-      <div class=" flex items-center  bg-flowlybg2 p-4 rounded-xl flex-[1_1_calc(25%-1rem)] min-w-[200px]">
-        <img src="/infoblock/3.png" class="max-w-[64px] max-h-[64px]" alt="">
-        <p class="text-lg text-white ml-4">Работает 24/7</p>
-      </div>
-      <div class=" flex items-center  bg-flowlybg2 p-4 rounded-xl flex-[1_1_calc(25%-1rem)] min-w-[200px]">
-        <img src="/infoblock/3.png" class="max-w-[64px] max-h-[64px]" alt="">
-        <p class="text-lg text-white ml-4">Обрабатывает текст</p>
-      </div>
-      <div class=" flex items-center  bg-flowlybg2 p-4 rounded-xl flex-[1_1_calc(25%-1rem)] min-w-[200px]">
-        <img src="/infoblock/4.png" class="max-w-[64px] max-h-[64px]" alt="">
-        <p class="text-lg text-white ml-4">Анализирует данные</p>
+      <div v-for="(feature, index) in features" :key="index" class="flex items-center bg-flowlybg2 p-4 rounded-xl flex-[1_1_calc(50%-1rem)] min-w-[200px] md:flex-[1_1_calc(25%-1rem)]">
+        <img :src="feature.img" class="max-w-[48px] md:max-w-[64px] max-h-[48px] md:max-h-[64px]" alt="">
+        <p class="text-lg text-white ml-4">{{ feature.text }}</p>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const features = [
+  { img: "/infoblock/4.png", text: "Распознает и генерирует голос" },
+  { img: "/infoblock/3.png", text: "Работает 24/7" },
+  { img: "/infoblock/3.png", text: "Обрабатывает текст" },
+  { img: "/infoblock/4.png", text: "Анализирует данные" }
+];
+</script>
